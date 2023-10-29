bc6c5b6fdcfdd25c35768988a414c6f0e2929acc
Matheus Pacheco Cruz <pachecx35@gmail.com>
2023-10-29 08:40:48 -0300

    up

---
+++ main.js
@@ -0,0 +1,13 @@
+const valorGorjeta = document.querySelector('#valor');
+const qntPessoa = document.querySelector('#pessoas');
+
+const btnCalcular = document.querySelector('.Calcular');
+const btnReset = document.querySelector('.Reset');
+
+
+btnCalcular.addEventListener('click', function(){
+    let valor = valorGorjeta.value
+    console.log(valor)
+
+})
+
