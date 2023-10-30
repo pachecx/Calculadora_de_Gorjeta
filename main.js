fc901ea10762523c238771853908cc5f170b7e55
Matheus Pacheco Cruz <pachecx35@gmail.com>
2023-10-30 08:10:14 -0300

    up

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
+//Inputs e botões
 const valorGorjeta = document.querySelector('#valor');
 const qntPessoa = document.querySelector('#pessoas');
-
 const btnCalcular = document.querySelector('.Calcular');
 const btnReset = document.querySelector('.Reset');
+const even = document.querySelector('.porcentagem');
+
+//Opções Porcentagem
+const cincoPor = document.querySelector('.cinco');
+const dezPor = document.querySelector('.dez');
+const quinPor = document.querySelector('.qui');
+const vintePor = document.querySelector('.vinte');
+const cinquPor = document.querySelector('.cinq');
 
 
-btnCalcular.addEventListener('click', function(){
-    let valor = valorGorjeta.value
-    console.log(valor)
-
+even.addEventListener('click',(event)=>{
+    event.preventDefault()
 })
 
+function Calcular(){
+    total = valorGorjeta.value
+    qnt = qntPessoa.value
+    valorPessoa = total / qnt
+
+    return valorPessoa;
+}
+
+function porcentagem(){
+    cinco = cincoPor.value
+    dez = dezPor.value 
+    quin = quinPor.value 
+    vinte = vintePor.value
+    cinque = cinquPor.value
+}
+
+btnCalcular.addEventListener('click', function(){
+    console.log(Calcular())
+})
+
+cinco.addEventListener('click', function(){
+    pagar = cinco * total
+    console.log(pagar)
+})
